92cd89c2177402de20d7f6caffd1011b9176a3b0
GRMams <dimamsjja@gmail.com>
2020-01-21 15:17:31 +0100

    #3 Added bidirectional option

--- src/Service/convertOperations.php
+++ src/Service/convertOperations.php
@@ -5,11 +5,19 @@ namespace App\Service;
 function m2toHectare($value)
 {
     return $value / 10000;
-
 }
 
 function kwToCo2($value)
 {
     return $value * 0.09;
+}
 
+function HectareToM2($value)
+{
+    return $value * 10000;
+}
+
+function co2ToKw($value)
+{
+    return $value / 0.09;
 }
